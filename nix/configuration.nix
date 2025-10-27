0bccdf05a6c4bec79511d183a078dbf3cf1ab9cc
Jenya <yyakovliev02@gmail.com>
2025-10-27 13:13:34 +0200

    test

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,39 +1,20 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, modulesPath, ... }:
 
 {
-  ########################################
-  # Boot setup (USB + load to RAM)
-  ########################################
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelParams = [ "toram" ];  # copies rootfs to RAM
-  boot.supportedFilesystems = [ "vfat" "ext4" "xfs" "zfs" ];
+  imports = [
+    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+  ];
 
-  ########################################
-  # Host + networking
-  ########################################
-  networking.hostName = "serenity-node";
+  boot.loader.systemd-boot.enable = true;
+  boot.kernelParams = [ "toram" ];
+
   networking.useDHCP = true;
 
-  ########################################
-  # User configuration
-  ########################################
   users.users.jenya = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ];    # sudo
-    initialPassword = "nixos";    # change after boot
+    extraGroups = [ "wheel" ];
+    initialPassword = "nixos";
   };
 
   security.sudo.enable = true;
-
-  ########################################
-  # Minimal environment
-  ########################################
-  environment.systemPackages = with pkgs; [ vim htop ];
-
-  ########################################
-  # Locale / time
-  ########################################
-  time.timeZone = "UTC";
-  i18n.defaultLocale = "en_US.UTF-8";
 }
--- nix/flake.nix
+++ nix/flake.nix
@@ -1,10 +1,10 @@
 {
-  description = "Minimal RAM-boot NixOS image";
+  description = "Serenity Node NixOS image";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
   outputs = { self, nixpkgs }: {
-    nixosConfigurations.ramboot = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.serenity-node = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./configuration.nix ];
     };
